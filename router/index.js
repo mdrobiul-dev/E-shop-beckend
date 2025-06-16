50ab131fc8c40fc18d8da84a0f5ad90ce5e3f930
its-robii <robiulhassanrobi33@gmail.com>
2025-06-16 21:01:54 +0600

    update

--- router/index.js
+++ router/index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const apiRouter = require('./api');
+const express = require("express");
+const apiRouter = require("./api");
 
-const router = express.Router()
+const router = express.Router();
 
-router.use("/api/v1", apiRouter)
+router.use("/api/v1", apiRouter);
 
-
-module.exports = router
+module.exports = router;
