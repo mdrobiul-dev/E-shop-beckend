428bdd6459a8e851f72477052dda2e6d24ee8e67
its-robii <robiulhassanrobi33@gmail.com>
2025-06-16 21:02:10 +0600

    update

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const dbConnect = require('./dbConfig/dbConnect');
-const router = require('./router');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const dbConnect = require("./dbConfig/dbConnect");
+const router = require("./router");
+require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.status(200).send("hello cghjgcb")
-  
-})
+  res.status(200).send("hello cghjgcb");
+});
 
 dbConnect();
 
 app.use(router);
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
-});  
-
+});
