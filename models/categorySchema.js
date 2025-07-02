1455e3dbec4965e2cfe6ac984483c035cc5b31e8
its-robii <robiulhassanrobi33@gmail.com>
2025-07-02 14:59:16 +0600

    controller validation

--- models/categorySchema.js
+++ models/categorySchema.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-const categorySchema = new Schema({
-    name : {
-        type : String,
-        required : true,
-        unique : true,
-        lowercase : true,
-        trim : true
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    image : {
-        type : String,
-        required : true
-    }
-},
-{
-    timestamps : true
-})
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("category", categorySchema)
+module.exports = mongoose.model("category", categorySchema);
