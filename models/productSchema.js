a8123583c371c6bafb606d1279807b5546fcaab8
its-robii <robiulhassanrobi33@gmail.com>
2025-07-02 20:48:07 +0600

    product schema

---
+++ models/productSchema.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; 
+
+const productSchema = new Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    category : {
+        type : Schema.Types.ObjectId,
+        ref : "category",
+        required : true
+    },
+    stock : {
+        type : Number,
+        required : true
+    },
+    status : {
+      type : String,
+      default : "pending",
+      enum : ["active","pending","rejected"]
+    },
+    mainImg : {
+        type : String,
+        required : true
+    },
+    images : [
+        {
+            type : String
+        }
+    ],
+    variants: [
+  {
+    name: {
+      type: String,
+      enum: ["color", "size"],
+      required: true,
+    },
+    options: [
+      {
+        value: { type: String, required: true },
+        additionalPrice: { type: Number, default: 0 },
+      }
+    ]
+  }
+]
+
+},
+  {
+    timestamps : true
+  }
+)
+
+module.exports = mongoose.model("product", productSchema)
