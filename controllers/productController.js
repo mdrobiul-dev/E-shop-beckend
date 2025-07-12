4aa7c16eb0b7a0839deca4a2e1a36131f615e975
its-robii <robiulhassanrobi33@gmail.com>
2025-07-12 21:22:28 +0600

    update

--- controllers/productController.js
+++ controllers/productController.js
@@ -136,4 +136,4 @@ const createProduct = async (req, res) => {
   res.status(200).send({ message: "Product created", product });
 };
 
-module.exports = createProduct;   
+module.exports = createProduct;      
