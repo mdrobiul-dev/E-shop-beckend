7433df881a545f3a99627970b477c67ce884086b
its-robii <robiulhassanrobi33@gmail.com>
2025-07-24 14:40:01 +0600

    cart update

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,7 +1,47 @@
-const addTocart = async (req, res) => {
-    
-    const { productID, quantity} = req.body;
-     res.status(200).send("hello world")
-}
+const cartSchema = require("../models/cartSchema");
 
-module.exports = {addTocart}
+const addTocart = async (req, res) => {
+  const { productID, quantity } = req.body;
+
+  if (!productID) {
+    return res.status(400).send({ error: "productID is required" });
+  }
+
+  const quantityNumber = parseInt(quantity) || 1;
+
+  try {
+    const userId = req.user._id;
+
+    let cart = await cartSchema.findOne({ user: userId });
+
+    if (!cart) {
+      cart = new cartSchema({
+        user: userId,
+        items: [],
+      });
+    }
+
+    const existingItem = cart.items.find(
+      (item) => item.product.toString() === productID
+    );
+
+    if (existingItem) {
+      existingItem.quantity += quantityNumber;
+      await cart.save();
+      return res.status(200).send({ message: "Cart item quantity updated", cart });
+    } else {
+      cart.items.push({
+        product: productID,
+        quantity: quantityNumber,
+      });
+      await cart.save();
+      return res.status(201).send({ message: "Item added to cart", cart });
+    }
+  } catch (error) {
+    console.error("Error fetching cart:", error);
+    res.status(500).send({ error: "Server error, try again" });
+  }
+};
+
+module.exports = { addTocart };
+
