0688b9c0ff2d77be6b04e31016279cbae71be48e
its-robii <robiulhassanrobi33@gmail.com>
2025-11-13 20:20:37 +0600

    update

--- router/api/index.js
+++ router/api/index.js
@@ -12,4 +12,5 @@ apiRouter.use("/product", productRoute );
 apiRouter.use("/cart", cartRouter)
 apiRouter.use("/order", orderRouter);
 
-module.exports = apiRouter;                                                     
+module.exports = apiRouter;                                                            
+ 
